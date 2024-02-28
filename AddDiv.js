dc23d0964520684d6003d389dd191b5d84c6eddf
Szadolowski <rafall.curzydlo@gmail.com>
2024-02-28 18:27:25 +0100

    Working Code which adding Task

--- AddDiv.js
+++ AddDiv.js
@@ -1,5 +1,55 @@
  const Add = document.getElementById("Add");
+ const WorkSpace = document.getElementById("WorkSpace");
+ const NewBlock = document.createElement("section");
+ const InnerHtml = `
+ <div>
+     <p>Name: </p>
+     <input type="text" id="AddName">
+ </div>
+ <div>
+     <p>Color: </p>
+     <input type="color" id="AddColor">
+ </div>
+ <div>
+     <p>Border: </p>
+     <input type="checkbox" id="AddBorder">
+ </div>
+ <div>
+     <p>Border Color: </p>
+     <input type="Color" id="AddBorderColor">
+ </div>
+ <input type="button" value="Create" id="NewSpace">
+`;
+ NewBlock.innerHTML = InnerHtml;
+ NewBlock.id = "Test";
 
  Add.addEventListener("click", function(){
-    alert("działa");
+   WorkSpace.appendChild(NewBlock);
+
+   let NewSpace = document.getElementById("NewSpace");
+
+   NewSpace.addEventListener("click", function(){
+      let Name = document.getElementById("AddName").value;
+      let Color = document.getElementById("AddColor").value;
+      let Border = document.getElementById("AddBorder").checked;
+      let BorderColor = document.getElementById("AddBorderColor").value;
+      console.log(Name+" "+Color+" "+Border+" "+BorderColor);
+   
+      let NewTask = document.createElement("section");
+      NewTask.id = Name;
+      NewTask.style.backgroundColor = Color;
+      NewTask.innerHTML = Name;
+      NewTask.style.width = "min-content";
+      NewTask.style.padding = "5px";
+      NewTask.style.borderRadius = "5px"
+   
+      if(Border){
+         console.log("test")
+         NewTask.style.border = "Solid 5px "+BorderColor;
+      }
+   
+      WorkSpace.appendChild(NewTask);
+      WorkSpace.removeChild(NewBlock);
+   
+    })
  })
